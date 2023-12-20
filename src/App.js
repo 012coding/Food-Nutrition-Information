be8dfc6517929d3b347be31e934c42105c5af8c0
김선욱 <tjsdnr960@naver.com>
2023-12-20 18:14:58 +0900

    영양성분 페이지 작성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import RecipePage from './routes/RecipePage';
 import RecipeDetail from './routes/RecipeDetail'
+import Nutrient from './routes/Nutrient';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         <Route path={`${process.env.PUBLIC_URL}/recipe`} element={<RecipePage />} />
         <Route path={`${process.env.PUBLIC_URL}/recipe/:id`} element={<RecipeDetail />} />
+        <Route path={`${process.env.PUBLIC_URL}/nutrient`} element={<Nutrient />} />
       </Routes>
     </Router>
   );
